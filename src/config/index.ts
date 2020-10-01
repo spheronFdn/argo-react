e2e85c22a549b6b4c3c89619541fc34c33c74012
Prashant Maurya <somitmaurya@gmail.com>
2020-10-02 02:02:04 +0530

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
-export const API_URL = "http://localhost:8080";
-export const BASE_URL = "http://localhost:3000";
+export const API_URL = "http://35.184.48.67:8080";
+export const BASE_URL = "http://35.184.48.67:3000";
 export const BACKEND_URL = "http://localhost:5000";
+
