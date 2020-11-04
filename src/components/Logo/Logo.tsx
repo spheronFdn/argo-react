7a108692156d5ecc180a0369d13373540de6d36d
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2020-11-04 23:23:36 +0530

    :sparkles: :bug: #100 Change logo from lazyloaded to normal

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { LazyLoadedImage } from "../_SharedComponents";
 import "./Logo.scss";
 import ILogoProps from "./model";
 
 const Logo: React.FC<ILogoProps> = ({ theme }) => {
   return (
     <div className="Logo">
-      <LazyLoadedImage height={42} once>
-        <img
-          src={require(theme === "light"
-            ? "../../assets/png/logo-colored.png"
-            : "../../assets/png/logo-white.png")}
-          alt={`ArGo ${theme === "light" ? "light" : "dark"} version`}
-          className="logo-image"
-          height={42}
-          loading="lazy"
-        />
-      </LazyLoadedImage>
+      <img
+        src={require(theme === "light"
+          ? "../../assets/png/logo-colored.png"
+          : "../../assets/png/logo-white.png")}
+        alt={`ArGo ${theme === "light" ? "light" : "dark"} version`}
+        className="logo-image"
+        height={42}
+        loading="lazy"
+      />
       <span
         className={`logo-text ${theme === "light" ? "logo-light" : "logo-dark"}`}
       >
