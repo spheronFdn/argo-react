e776de711621e4cff13227e3f59ef7cb17834ed4
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2021-06-29 14:12:30 +0530

    feat: updated config and biconomy stuff

--- src/components/DeploySiteConfig/DeploySiteConfig.tsx
+++ src/components/DeploySiteConfig/DeploySiteConfig.tsx
@@ -666,26 +666,22 @@ function DeploySiteConfig() {
                         </div>
                         {framework !== "static" && (
                           <>
-                            {framework !== "next" ? (
-                              <div className="deploy-site-item-form-item">
-                                <label>Package Manager</label>
-                                <div className="deploy-site-item-select-container">
-                                  <select
-                                    className="deploy-site-item-select"
-                                    value={packageManager}
-                                    onChange={(e) =>
-                                      setPackageManager(e.target.value)
-                                    }
-                                  >
-                                    <option value="npm">NPM</option>
-                                    <option value="yarn">YARN</option>
-                                  </select>
-                                  <span className="select-down-icon">
-                                    <FontAwesomeIcon icon={faChevronDown} />
-                                  </span>
-                                </div>
+                            <div className="deploy-site-item-form-item">
+                              <label>Package Manager</label>
+                              <div className="deploy-site-item-select-container">
+                                <select
+                                  className="deploy-site-item-select"
+                                  value={packageManager}
+                                  onChange={(e) => setPackageManager(e.target.value)}
+                                >
+                                  <option value="npm">NPM</option>
+                                  <option value="yarn">YARN</option>
+                                </select>
+                                <span className="select-down-icon">
+                                  <FontAwesomeIcon icon={faChevronDown} />
+                                </span>
                               </div>
-                            ) : null}
+                            </div>
                             <div className="deploy-site-item-form-item">
                               <label>Build command</label>
                               {framework !== "next" ? (
